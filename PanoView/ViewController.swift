959e58dea388782765006f81cd18bca56ef63892
Jeff Schmitz <jeffeschmitz@gmail.com>
2016-10-12 07:40:52 -0500

    GMSPanoramaViewDelegate working with single view controller

--- PanoView/ViewController.swift
+++ PanoView/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GMSPanoramaViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let panoView = GMSPanoramaView(frame: CGRect.zero)
+        self.view = panoView
+        
+        panoView.moveNearCoordinate(CLLocationCoordinate2DMake(-33.732, 150.312))
+        panoView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - GMSPanoramaViewDelegate
+    func panoramaView(_ view: GMSPanoramaView, error: Error, onMoveNearCoordinate coordinate: CLLocationCoordinate2D) {
+        print("Moving near coordinate (\(coordinate.latitude),\(coordinate.longitude) error: \(error.localizedDescription)")
+    }
 
+    func panoramaView(_ view: GMSPanoramaView, error: Error, onMoveToPanoramaID panoramaID: String) {
+        print("Moving to PanoID \(panoramaID) error: \(error.localizedDescription)")
+    }
+    
+    func panoramaView(_ view: GMSPanoramaView, didMoveTo panorama: GMSPanorama?) {
+        print("Moved to panoramaID: \(panorama?.panoramaID) coordinates: (\(panorama?.coordinate.latitude),\(panorama?.coordinate.longitude))")
+    }
 }
 
